aeb2c88a4dcc93e49d0511d47194c98ab94e88ea
jeyden94 <jeyden94@gmail.com>
2025-07-12 20:34:52 -0500

    Completed etch-a-sketch project tasks

--- dom.js
+++ dom.js
@@ -6,15 +6,27 @@ containerRowTemplate.classList.add("container-row");
 const gridBoxTemplate = document.createElement("div");
 gridBoxTemplate.classList.add("grid-box");
 
-for (let i = 0; i < 16; i++) {
-    
-    const containerRow = containerRowTemplate.cloneNode(true)
+function createGrid () {
+    container.innerHTML = ""
+    let squares = prompt("How many square per side of the new grid?", "#")
+    if (squares < 101) {
+        for (let i = 0; i < squares; i++) {
+            
+            const containerRow = containerRowTemplate.cloneNode(true)
+            // containerRow.classList.add(`container-row-${i+1}`)
+            container.appendChild(containerRow)
 
-    container.appendChild(containerRow)
-
-    for (let j = 0; j < 16; j++) {
-        const gridBox = gridBoxTemplate.cloneNode(true);
-        containerRow.appendChild(gridBox)
-    }
+            for (let j = 0; j < squares; j++) {
+                const gridBox = gridBoxTemplate.cloneNode(true);
+                // gridBox.classList.add(`grid-box-${j+1}-row-${i+1}`)
+                gridBox.addEventListener("mouseenter", () => gridBox.style.backgroundColor = "black")
+                containerRow.appendChild(gridBox)
+            }
+        } 
+    } else {
+        alert("Input must be less than 100")
+    } 
 
 }
+
+
